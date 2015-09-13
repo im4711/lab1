5b4b67e6886569bc72c1229c471d58d7a2b218e0
Ignacio Marquez <igsmar@gmail.com>
2015-09-13 14:27:09 -0700

    student comments

--- Student.php
+++ Student.php
@@ -7,7 +7,7 @@
  */
 
 /**
- * Description of Student
+ * A student class
  *
  * @author a00848501
  */
@@ -19,14 +19,27 @@ class Student {
         $this->grades = array();
     }
     
+    /**
+     * adds an email to a particular student's list of email addresses
+     * @param type $which
+     * @param type $address
+     */
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
     
+    /**
+     * adds a grade to a particular student's list of grades
+     * @param type $grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    /**
+     * averages the students grades
+     * @return type
+     */
     function average() {
         $total = 0;
         foreach($this->grades as $value)
@@ -34,6 +47,11 @@ class Student {
         return $total / count($this->grades);
     }
     
+    /**
+     * displays a formatted string detailing a student's name, email, average
+     * grade
+     * @return type
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' (' . $this->average() . ")\n";
